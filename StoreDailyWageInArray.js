18e05f78f7825e8326d0c097d76e636d110d29c8
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-06-09 22:50:43 +0530

    Added: Ability to store daily wage in an array

---
+++ StoreDailyWageInArray.js
@@ -0,0 +1,43 @@
+const NO_TIME = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+const WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+let empDailyWageArray = new Array();
+let totalWage = 0;
+
+function calcWagesForAMonth() {
+  let totalEmpHrs = 0;
+  let days=0;
+
+  while(days<WORKING_DAYS && totalEmpHrs<=MAX_HRS_IN_MONTH){
+    let empType = Math.floor(Math.random() * 3);
+    totalEmpHrs+=getWorkingHrs(empType)
+    empDailyWageArray.push(calculateWage(getWorkingHrs(empType)));
+    days++;
+  }
+
+  let empWageForMonth = totalEmpHrs * WAGE_PER_HR;
+  console.log(`    Total working days = ${days} 
+    Total Hrs = ${totalEmpHrs}
+    Total Wage for Month = ${empDailyWageArray.reduce((totalWage, dailyWage) => totalWage + dailyWage)}`)
+
+  return empWageForMonth;
+}
+
+function calculateWage(empHrs){
+  return WAGE_PER_HR * empHrs
+}
+
+function getWorkingHrs(empType) {
+  switch (empType) {
+    case IS_PART_TIME: return PART_TIME_HRS;
+    case IS_FULL_TIME: return FULL_TIME_HRS;
+    default: return 0;
+  }
+}
+
+calcWagesForAMonth();
