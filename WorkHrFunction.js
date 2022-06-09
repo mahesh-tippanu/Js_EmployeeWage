c90203f0a2e8122fedd9640b0ce5a26516d51d02
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-06-09 22:45:52 +0530

    Refactored: created a function to get work hrs

---
+++ WorkHrFunction.js
@@ -0,0 +1,20 @@
+const NO_TIME = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+
+let empType = Math.floor(Math.random() * 3);
+
+function getWorkingHrs(empType) {
+  switch (empType) {
+    case IS_PART_TIME: return PART_TIME_HRS;
+    case IS_FULL_TIME: return FULL_TIME_HRS;
+    default: return 0;
+  }
+}
+
+let empHrs = getWorkingHrs(empType);
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Emp Wage: " + empWage);
